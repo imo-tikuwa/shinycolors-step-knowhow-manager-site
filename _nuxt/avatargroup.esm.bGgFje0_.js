ce3fff5b3574d366634121c394ab67f10201a2a8
imo-tikuwa <48991931+imo-tikuwa@users.noreply.github.com>
2023-12-26 14:14:59 +0000

    Commit the result of `nuxt generate`

---
+++ _nuxt/avatargroup.esm.bGgFje0_.js
@@ -0,0 +1,12 @@
+import{a9 as r,ak as e,t,v as n,W as s,al as p}from"./entry.X88aA8yG.js";var o=`
+@layer primevue {
+    .p-avatar-group .p-avatar + .p-avatar {
+        margin-left: -1rem;
+    }
+
+    .p-avatar-group {
+        display: flex;
+        align-items: center;
+    }
+}
+`,v={root:"p-avatar-group p-component"},l=r.extend({name:"avatargroup",css:o,classes:v}),c={name:"BaseAvatarGroup",extends:e,style:l,provide:function(){return{$parentInstance:this}}},i={name:"AvatarGroup",extends:c};function u(a,d,m,g,$,f){return t(),n("div",p({class:a.cx("root")},a.ptm("root"),{"data-pc-name":"avatargroup"}),[s(a.$slots,"default")],16)}i.render=u;export{i as default};
