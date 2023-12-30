a3324f4cee6733a712dddeabd6685ecc519906b1
imo-tikuwa <48991931+imo-tikuwa@users.noreply.github.com>
2023-12-30 12:35:24 +0000

    Commit the result of `nuxt generate`

--- _nuxt/tabpanel.esm.rFyjjcTM.js
+++
@@ -1,1 +0,0 @@
-import{al as l,Z as a}from"./entry.AMNS2Co5.js";var n={},r={name:"BaseTabPanel",extends:l,props:{header:null,headerStyle:null,headerClass:null,headerProps:null,headerActionProps:null,contentStyle:null,contentClass:null,contentProps:null,disabled:Boolean},style:n},t={name:"TabPanel",extends:r};function s(e,o,d,p,u,c){return a(e.$slots,"default")}t.render=s;export{t as default};
